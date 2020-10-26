0f130ecd359469d84b5579b9de3056a3cf7c6748
Jan Willem <janwillembuist@gmail.com>
2020-10-26 15:24:50 +0100

    Added zeros to uncertain classification intervals

--- README.md
+++ README.md
@@ -4,9 +4,10 @@ PIP-FUCCI tool in MATLAB
 ## TODO
 1. ~~Add SiR-DNA channel~~
 2. Set classifier to zero if it is the first or last cell stage
+3. Find out the useless functions and remove
+3. Migrate the tool from GUIDE to new GUI options in MATLAB
 3. Add image in GUI for visual inspection
 3. Improve automatic detection
-3. Migrate the tool from GUIDE to new GUI options in MATLAB
 4. Enable csv loading as input, code speed gains, error debugging
 
 ## BUGS
--- test.m
+++ test.m
@@ -1,1 +1,27 @@
-H = FUCCI_V4('gui_Name', 'test');
+a = [1, 1, 1, 2, 2, 4, 4]
+b = delete_begin_end(b)
+
+function [classified] = delete_begin_end(classified)
+% Function that deletes the begin and end of a certain classification. It
+% puts a zero for deleted classification. (JWB)
+first = classified(1);
+last = classified(end);
+
+% Loop over entries from second item 
+for i=1:1:length(classified)
+    if classified(i) ~= first
+        % A classification boundary is reached
+        break
+    end
+    classified(i) = 0;
+end
+
+% Loop backwards over entries from second to last item
+for i=length(classified):-1:1
+    if classified(i) ~= last
+        % A classification boundary is reached
+        break
+    end
+    classified(i) = 0;
+end
+end
